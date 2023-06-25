972ba0a12c5ff80a4f810c5fd98ab033ed1d8e79
Knosal.V <35274816+knosal@users.noreply.github.com>
2023-06-25 07:14:20 +0300

    Изменил тип сортировки. Убрал js

--- source/js/script.js
+++ source/js/script.js
@@ -2,10 +2,8 @@
 import { mapDisplay, mapDisplayNoJs } from './map.js';
 import { initButtonMenu } from './popup-menu.js';
 import { initRangeSlider } from './range-slider.js';
-import { initSelect } from './select.js';
 //import { initSliderSwiper } from './slider-swiper.js';
 
-initSelect();
 mapDisplayNoJs();
 mapDisplay();
 initButtonMenu();
